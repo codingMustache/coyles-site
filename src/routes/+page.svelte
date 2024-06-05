04a15d8356f29414d04f9848d124ee4b2674c021
Jorge <jorge@codingMustache.dev>
2024-06-04 22:39:11 -0500

    setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import '../reset.css';
+	import IG from '$lib/instagram.svelte';
+	import Email from '$lib/email.svelte';
+</script>
+
+<main>
+	<div class="page-content">
+		<p>Come Back Later</p>
+		<p>Under Construction</p>
+		<div class="links">
+			<a href="https://www.instagram.com/coyle_parker_?igsh=Nm1hemFxc294ZDJv">
+				<button> <IG /> IG </button>
+			</a>
+			<a href="mailto:erniepthethrid@gmail.com">
+				<button> <Email /> Email </button>
+			</a>
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		background: #0000 center / cover no-repeat url('coyle.webp');
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.page-content {
+		background-color: rgba(82, 86, 88, 0.512);
+		text-align: center;
+		padding: 10%;
+		font-size: larger;
+		color: white;
+		border-radius: 5px;
+	}
+	button {
+		display: flex;
+		align-items: center;
+		margin: 10px;
+		padding: 5px;
+		gap: 10px;
+		border-radius: 5px;
+		border: unset;
+		box-shadow: 3px 3px 10px black;
+	}
+	.links {
+		display: flex;
+		align-items: center;
+	}
+</style>
