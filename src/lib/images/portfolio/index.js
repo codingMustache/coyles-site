65adb3ce8535f5a769c3ef63fa7e78ef0d632bbb
Jorge <jorge@codingMustache.dev>
2024-06-17 14:06:42 -0500

    changed those photos to what coyle was looking for

--- src/lib/images/portfolio/index.js
+++ src/lib/images/portfolio/index.js
@@ -1,22 +1,29 @@
-import img1 from './img1.webp';
-import img2 from './img2.webp';
-import img3 from './img3.webp';
-import img4 from './img4.webp';
-import img5 from './img5.webp';
-import img6 from './img6.webp';
-import img7 from './img7.webp';
-import img8 from './img8.webp';
-import img9 from './img9.webp';
-import img10 from './img10.webp';
-import img11 from './img11.webp';
-import img12 from './img12.webp';
-import img13 from './img13.webp';
-import img14 from './img14.webp';
-import img15 from './img15.webp';
-import img16 from './img16.webp';
-import img17 from './img17.webp';
-import img18 from './img18.webp';
-import img19 from './img19.webp';
-import img20 from './img20.webp';
+import img1 from './img-1.webp';
+import img2 from './img-2.webp';
+import img3 from './img-3.webp';
+import img4 from './img-4.webp';
+import img5 from './img-5.webp';
+import img6 from './img-6.webp';
+import img7 from './img-7.webp';
+import img8 from './img-8.webp';
+import img9 from './img-9.webp';
+import img10 from './img-10.webp';
+import img11 from './img-11.webp';
+import img12 from './img-12.webp';
+import img13 from './img-13.webp';
+import img14 from './img-14.webp';
+import img15 from './img-15.webp';
+import img16 from './img-16.webp';
+import img17 from './img-17.webp';
+import img18 from './img-18.webp';
+import img19 from './img-19.webp';
 
-export default [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20]
+
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19]
+
+const randomImages = new Set();
+while (randomImages.size < 10) {
+  randomImages.add(images[Math.floor(Math.random() * images.length)]);
+}
+
+export default [...randomImages];
