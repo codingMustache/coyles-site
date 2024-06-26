b3bf9e2fce178c730093679a4ef2174fd42e7b58
Jorge <jorge@codingMustache.dev>
2024-06-26 16:17:21 -0500

    added reviewers

--- src/lib/components/review-list.svelte
+++ src/lib/components/review-list.svelte
@@ -8,14 +8,28 @@
 			img: jorge
 		},
 		{
-			name: 'John Doe',
-			review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-			img: headshot
+			name: 'Owen Weber',
+			review:
+				'I have been going to Coyle for 6-7 years now. He is a great artist and person. I always look foreword to appts with him.',
+			img: 'https://lh3.googleusercontent.com/a-/ALV-UjWijk66ij6z7N2HOp_bG-dsQg1rLJTZ03ArxQKF2jx694gpjHJh=s40-c-rp-mo-br100'
 		},
 		{
-			name: 'Jane Doe',
-			review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-			img: headshot
+			name: 'Jena Renee',
+			review:
+				'I am a repeat customer and Coyle has never let me down! Personally, I love to give all creative freedom to the artist tattooing me. And with Coyle’s unique style, you know you won’t see anyone else walking around with your same piece! Highly recommend him!',
+			img: 'https://lh3.googleusercontent.com/a-/ALV-UjV6Fkg5kogP5jYL5w4QwuH6XzX7ANGziy4QYLaFuPBj5pjffOfU=s40-c-rp-mo-br100'
+		},
+		{
+			name: 'Michelle Villarrubia',
+			review:
+				'I’ve been tattooed by Coyle multiple times and it’s always a great experience! He’s creative, does fantastic work, and makes the overall experience easy and welcoming.',
+			img: 'https://lh3.googleusercontent.com/a-/ALV-UjVr0fHhKq97FPtEFrmug6AhOJDKpoIbRxG-9rZ0ORIIkDX0a10FCg=s40-c-rp-mo-br100'
+		},
+		{
+			name: 'Elias Hinderberger',
+			review:
+				'I’ve had two pieces from Coyle, and I’ve been really happy with both. Great communication while coming up with custom designs, and beautiful execution. I’ll be going back to have him do a handful more for sure',
+			img: 'https://lh3.googleusercontent.com/a-/ALV-UjVcTjPflPoI7R4hAlHDB6aDOp-Vd7O-wxFxXLxXwsY3Lgh1_GP-=w60-h60-p-rp-mo-br100'
 		}
 	];
 </script>
