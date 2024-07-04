28b13a6607602e35251a0f098eebd5b5700cdbcb
Jorge <jorge@codingMustache.dev>
2024-07-04 10:34:14 -0500

    fixed issue

--- src/routes/(app)/contact/+page.svelte
+++ src/routes/(app)/contact/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import FormModal from '$lib/components/please-read.svelte';
-	import img from '$lib/images/headshot.jpeg';
+
 	import ContactForm from '$lib/components/contact-form.svelte';
 </script>
 
@@ -8,7 +8,6 @@
 	<title>Coyle Parker</title>
 	<meta property="og:title" content="Book you next tattoo with Coyle Parker" />
 	<meta property="og:type" content="website" />
-	<meta property="og:image" content={img} />
 	<meta property="og:url" content="https://www.coyleparker.art/contact" />
 </svelte:head>
 
