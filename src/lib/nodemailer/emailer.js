940d93c1c2f118445f426ed3de64096923e1488d
Jorge <contact@codingmustache.dev>
2024-07-07 04:11:34 -0500

    fixed email return maybe

--- src/lib/nodemailer/emailer.js
+++ src/lib/nodemailer/emailer.js
@@ -16,12 +16,38 @@ const transporter = nodemailer.createTransport({
  */
 export const emailer = async (msg, attachments = []) => {
 	// send mail with defined transport object
-	const info = await transporter.sendMail({
+
+	await new Promise((resolve, reject) => {
+		// verify connection configuration
+		transporter.verify(function (error, success) {
+			if (error) {
+				console.log(error);
+				reject(error);
+			} else {
+				console.log('Server is ready to take our messages');
+				resolve(success);
+			}
+		});
+	});
+
+	const mailData = {
 		// @ts-ignore
 		from: `Coyle Parker <appt@coyleparker.art>`,
 		to: 'test@codingMustache.dev',
 		subject: 'New Client',
 		html: msg,
 		attachments: attachments
+	};
+	await new Promise((resolve, reject) => {
+		// send mail
+		transporter.sendMail(mailData, (err, info) => {
+			if (err) {
+				console.error(err);
+				reject(err);
+			} else {
+				console.log(info);
+				resolve(info);
+			}
+		});
 	});
 };
